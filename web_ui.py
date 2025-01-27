354f487bb2b87a043ee764fbaf5d0368decb8dab
xkrissgaj <250410@student.pwr.edu.pl>
2025-01-27 15:21:46 +0000

    renamed:    data/213769-Wroclaw.json -> data/213796-Wroclaw.json 	new file:   data/259896-OSAKA.json 	new file:   data/259910-10890.json 	new file:   data/259910-2163391.json 	new file:   data/261334-Tokyo.json 	new file:   data/261833-Del Norte.json 	new file:   data/262081-6161.json 	modified:   docker-compose.yml 	modified:   mqtt_subscriber.py 	new file:   test.py 	modified:   web_ui.py

--- web_ui.py
+++ web_ui.py
@@ -2,33 +2,44 @@ from flask import Flask, render_template
 import os
 import json
 
-# Inicjalizacja aplikacji Flask
 app = Flask(__name__)
 
-# Ścieżka do folderu z danymi (współdzielona przez Docker Volume)
 DATA_FOLDER = os.getenv("DATA_FOLDER", "./data")
 
 @app.route('/')
 def index():
     try:
-        # Pobierz listę plików z folderu DATA_FOLDER
         files = os.listdir(DATA_FOLDER)
-        
-        # Lista do przechowywania danych
         measurements = []
 
         for file in files:
             if file.endswith('.json'):
                 file_path = os.path.join(DATA_FOLDER, file)
 
-                with open(file_path, 'r') as f:
-                    data = json.load(f)
+                try:
+                    with open(file_path, 'r') as f:
+                        data = json.load(f)
+                        if isinstance(data, dict):
+                            measurements.append({
+                                "file": file,
+                                "data": data
+                            })
+                        else:
+                            measurements.append({
+                                "file": file,
+                                "data": {"error": "Nieprawidłowy format danych (nie jest słownikiem)."}
+                            })
+                except json.JSONDecodeError:
                     measurements.append({
                         "file": file,
-                        "data": data
+                        "data": {"error": "Nieprawidłowy format pliku JSON."}
+                    })
+                except Exception as e:
+                    measurements.append({
+                        "file": file,
+                        "data": {"error": f"Błąd: {str(e)}"}
                     })
 
-        # Renderuj stronę HTML z wykorzystaniem Jinja2
         return render_template('index.html', measurements=measurements)
 
     except Exception as e:
